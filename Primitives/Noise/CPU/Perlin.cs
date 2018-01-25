0f0488405ad2b46a041d40f118727dba62aaf625
Colin Halseth <qkmaxware@gmail.com>
2018-01-24 19:43:52 -0800

    Simple elliptical orbits

--- Primitives/Noise/CPU/Perlin.cs
+++ Primitives/Noise/CPU/Perlin.cs
@@ -4,129 +4,141 @@ using UnityEngine;
 
 namespace Spaceworks {
 
-	public class Perlin : INoise{
+    public class Perlin : INoise {
 
-		private static readonly int[] p = new int[512];
-		private static readonly int[] permutation = new int[]{151, 160, 137, 91, 90, 15,
-			131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
-			190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
-			88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166,
-			77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
-			102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196,
-			135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
-			5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
-			223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
-			129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228,
-			251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
-			49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
-			138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
-		};
+        private static readonly int[] p = new int[512];
+        private static readonly int[] permutation = new int[]{151, 160, 137, 91, 90, 15,
+            131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
+            190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
+            88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166,
+            77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
+            102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196,
+            135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
+            5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
+            223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
+            129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228,
+            251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
+            49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
+            138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
+        };
 
-		static Perlin(){
-			for (int i = 0; i < 256; i++) {
-				p[i] = permutation[i];
-				p[256 + i] = p[i];
-			}
-		}
+        static Perlin() {
+            for (int i = 0; i < 256; i++) {
+                p[i] = permutation[i];
+                p[256 + i] = p[i];
+            }
+        }
 
-		/// <summary>
-		/// Evaluate the quintic funtion at t
-		/// </summary>
-		/// <param name="t">T.</param>
-		private static double quintic(double t) {
-			return t * t * t * (t * (t * 6 - 15) + 10);
-		}
+        /// <summary>
+        /// Evaluate the quintic funtion at t
+        /// </summary>
+        /// <param name="t">T.</param>
+        private static double quintic(double t) {
+            return t * t * t * (t * (t * 6 - 15) + 10);
+        }
 
-		/// <summary>
-		/// Evaluate the derivative of the quintic function at t
-		/// </summary>
-		/// <returns>The derivative.</returns>
-		/// <param name="t">T.</param>
-		private static double quinticDerivative(double t){
-			return 30 * t * t * (t * (t - 2) + 1);
-		}
+        /// <summary>
+        /// Evaluate the derivative of the quintic function at t
+        /// </summary>
+        /// <returns>The derivative.</returns>
+        /// <param name="t">T.</param>
+        private static double quinticDerivative(double t) {
+            return 30 * t * t * (t * (t - 2) + 1);
+        }
 
-		/// <summary>
-		/// Compute the gradient of x,y,z based on a hash value
-		/// </summary>
-		/// <param name="hash">Hash.</param>
-		/// <param name="x">The x coordinate.</param>
-		/// <param name="y">The y coordinate.</param>
-		/// <param name="z">The z coordinate.</param>
-		private static double grad(int hash, double x, double y, double z) {
-			int h = hash & 15;                      // CONVERT LO 4 BITS OF HASH CODE
-			double u = h < 8 ? x : y, // INTO 12 GRADIENT DIRECTIONS.
-			v = h < 4 ? y : h == 12 || h == 14 ? x : z;
-			return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
-		}
+        /// <summary>
+        /// Compute the gradient of x,y,z based on a hash value
+        /// </summary>
+        /// <param name="hash">Hash.</param>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="z">The z coordinate.</param>
+        private static double grad(int hash, double x, double y, double z) {
+            int h = hash & 15;                      // CONVERT LO 4 BITS OF HASH CODE
+            double u = h < 8 ? x : y, // INTO 12 GRADIENT DIRECTIONS.
+            v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+            return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+        }
 
-		/// <summary>
-		/// Linearly interpolate two floats
-		/// </summary>
-		/// <param name="t">T.</param>
-		/// <param name="a">The alpha component.</param>
-		/// <param name="b">The blue component.</param>
-		private static double lerp(double t, double a, double b) {
-			return a + t * (b - a);
-		}
+        /// <summary>
+        /// Linearly interpolate two floats
+        /// </summary>
+        /// <param name="t">T.</param>
+        /// <param name="a">The alpha component.</param>
+        /// <param name="b">The blue component.</param>
+        private static double lerp(double t, double a, double b) {
+            return a + t * (b - a);
+        }
 
-		/// <summary>
-		/// Fast version of Math.Floor
-		/// </summary>
-		/// <param name="x">The x coordinate.</param>
-		private static int fastfloor(double x) {
-			return x>0 ? (int)x : (int)x-1;
-		}
+        /// <summary>
+        /// Fast version of Math.Floor
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        private static int fastfloor(double x) {
+            return x > 0 ? (int)x : (int)x - 1;
+        }
 
-		/// <summary>
-		/// Perlin noise at position x,y
-		/// </summary>
-		/// <param name="x">The x coordinate.</param>
-		/// <param name="y">The y coordinate.</param>
-		public static double Noise(double x, double y){
-			return Noise(x,y,0);
-		}
+        /// <summary>
+        /// Perlin noise at position x,y
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        public static double Noise(double x, double y) {
+            return Noise(x, y, 0);
+        }
 
-		/// <summary>
-		/// Perlin noise value at position x,y,z
-		/// </summary>
-		/// <param name="x">The x coordinate.</param>
-		/// <param name="y">The y coordinate.</param>
-		/// <param name="z">The z coordinate.</param>
-		private static double Noise(double x, double y, double z) {
-			int X = fastfloor(x) & 255;
-			int Y = fastfloor(y) & 255;
-			int Z = fastfloor(z) & 255;
+        /// <summary>
+        /// Perlin noise value at position x,y,z
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="z">The z coordinate.</param>
+        private static double Noise(double x, double y, double z) {
+            int X = fastfloor(x) & 255;
+            int Y = fastfloor(y) & 255;
+            int Z = fastfloor(z) & 255;
 
-			x -= fastfloor(x);
-			y -= fastfloor(y);
-			z -= fastfloor(z);
+            x -= fastfloor(x);
+            y -= fastfloor(y);
+            z -= fastfloor(z);
 
-			double u = quintic(x);
-			double v = quintic(y);
-			double w = quintic(z);
+            double u = quintic(x);
+            double v = quintic(y);
+            double w = quintic(z);
 
-			int A = p[X] + Y;
-			int AA = p[A] + Z;
-			int AB = p[A + 1] + Z;
-			int B = p[X + 1] + Y;
-			int BA = p[B] + Z;
-			int BB = p[B + 1] + Z;
+            int A = p[X] + Y;
+            int AA = p[A] + Z;
+            int AB = p[A + 1] + Z;
+            int B = p[X + 1] + Y;
+            int BA = p[B] + Z;
+            int BB = p[B + 1] + Z;
 
-			return lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z), // AND ADD
-				grad(p[BA], x - 1, y, z)), // BLENDED
-				lerp(u, grad(p[AB], x, y - 1, z), // RESULTS
-					grad(p[BB], x - 1, y - 1, z))),// FROM  8
-				lerp(v, lerp(u, grad(p[AA + 1], x, y, z - 1), // CORNERS
-					grad(p[BA + 1], x - 1, y, z - 1)), // OF CUBE
-					lerp(u, grad(p[AB + 1], x, y - 1, z - 1),
-						grad(p[BB + 1], x - 1, y - 1, z - 1))));
-		}
+            return lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z), // AND ADD
+                grad(p[BA], x - 1, y, z)), // BLENDED
+                lerp(u, grad(p[AB], x, y - 1, z), // RESULTS
+                    grad(p[BB], x - 1, y - 1, z))),// FROM  8
+                lerp(v, lerp(u, grad(p[AA + 1], x, y, z - 1), // CORNERS
+                    grad(p[BA + 1], x - 1, y, z - 1)), // OF CUBE
+                    lerp(u, grad(p[AB + 1], x, y - 1, z - 1),
+                        grad(p[BB + 1], x - 1, y - 1, z - 1))));
+        }
 
-		public float Value (Vector3 pos, float freq){
-			return (float)Noise (pos.x * freq, pos.y * freq, pos.z * freq);
-		}
+        /// <summary>
+        /// Perlin noise value, Range -1.0 : 1.0
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="freq"></param>
+        /// <returns></returns>
+        public float Value(Vector3 pos, float freq) {
+            return (float)Noise(pos.x * freq, pos.y * freq, pos.z * freq);
+        }
 
+        /// <summary>
+        /// FBM Sum value, Range -1.0 : 1.0
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="opts"></param>
+        /// <returns></returns>
 		public float Sum (Vector3 pos, NoiseOptions opts){
 			//Setup
 			double total = 0.0f;
@@ -158,6 +170,51 @@ namespace Spaceworks {
 			return noise;
 		}
 
-	}
+        private double Ridge(double x, double offset = 0) {
+            x = System.Math.Abs(x);
+            x = offset - x;
+            x *= x;
+            return x;
+        }
+
+        /// <summary>
+        /// Ridged multifractal value. Range 0.0 : 1.0
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="opts"></param>
+        /// <returns></returns>
+        public float Ridged(Vector3 pos, NoiseOptions opts) {
+            //Setup
+            double total = 0.0f;
+            float amplitude = 0.5f;
+            double previous = 1.0f;
+
+            float freq = opts.frequency;
+
+            //FastRandom rng = new FastRandom (opts.seed);
+
+            //Value is between -1 an 1
+            for (int i = 0; i < opts.octaves; i++) {
+                //int sx = rng.Next(-10000, 10000);
+                //int sy = rng.Next(-10000, 10000);
+                //int sz = rng.Next(-10000, 10000);
+
+                double value = Noise(
+                    (pos.x + opts.offset.x) * freq - opts.seed,
+                    (pos.y + opts.offset.y) * freq - opts.seed,
+                    (pos.z + opts.offset.z) * freq - opts.seed
+                );
+
+                total +=  Ridge(value)* amplitude * previous;
+                previous = value;
+                freq *= opts.lacunarity;
+                amplitude *= opts.persistence;
+            }
+
+            //Return the processed value
+            return (float)total;
+        }
+
+    }
 
 }
---
+++ Systems/Orbits/SimpleOrbit.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spaceworks {
+
+    public class SimpleOrbit : MonoBehaviour {
+
+        public enum FocalPoint {
+            NegativeMajorAxis, PositiveMajorAxis
+        }
+
+        [Header("Ellipse Shape")]
+        public float semiMajorAxis;
+        [Range(0,1)]
+        public float eccentricity;
+        public Vector3 rotation;
+
+        public float a {
+            get {
+                return semiMajorAxis;
+            }
+        }
+
+        public float b {
+            get {
+                return a * Mathf.Sqrt(1  -eccentricity * eccentricity);
+            }
+        }
+
+        [Header("Ellipse Positioning")]
+        public FocalPoint focusPoint = FocalPoint.NegativeMajorAxis;
+        public Transform focusObject;
+
+        [Header("Body Parameters")]
+        [Range(0, 2 * Mathf.PI)]
+        public float startPosition;
+        public float orbitalVelocity;
+
+        //Internal values
+        private float _position;
+        private Vector3 savedRotation;
+        private Quaternion _rot = Quaternion.identity;
+
+        public Vector3 focusNegative {
+            get {
+                if (focusPoint == FocalPoint.NegativeMajorAxis)
+                    return focusObject.position;
+
+                Vector3 dirToCenterFromFoci = _rot * ((focusPoint == FocalPoint.NegativeMajorAxis) ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0));
+                float distToCenter = Mathf.Sqrt(a * a - b * b);
+
+                return focusObject.position + dirToCenterFromFoci * 2 * distToCenter;
+            }
+        }
+
+        public Vector3 focusPositive {
+            get {
+                if (focusPoint == FocalPoint.PositiveMajorAxis)
+                    return focusObject.position;
+
+                Vector3 dirToCenterFromFoci = _rot * ((focusPoint == FocalPoint.NegativeMajorAxis) ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0));
+                float distToCenter = Mathf.Sqrt(a * a - b * b);
+
+                return focusObject.position + dirToCenterFromFoci * 2 * distToCenter;
+            }
+        }
+
+        public Vector3 center {
+            get {
+                Vector3 dirToCenterFromFoci = _rot * ((focusPoint == FocalPoint.NegativeMajorAxis) ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0));
+                float distToCenter = Mathf.Sqrt(a * a - b * b);
+
+                return focusObject.position + dirToCenterFromFoci * distToCenter;
+            }
+        }
+
+        void Start() {
+            _position = this.startPosition;
+            this.transform.position = Evaluate(_position);
+            _position = Mathf.Repeat(_position, 2 * Mathf.PI);
+        }
+
+        void Update() {
+            _position += this.orbitalVelocity * Time.deltaTime;
+            this.transform.position = Evaluate(_position);
+        }
+
+        public Vector3 Evaluate(float f) {
+            f = Mathf.Repeat(f, 2 * Mathf.PI);
+
+            //Cache rotation
+            if (savedRotation != rotation) {
+                savedRotation = rotation;
+                _rot = Quaternion.Euler(savedRotation);
+            }
+
+            Vector3 location = new Vector3(
+                a * Mathf.Cos(f),
+                0,
+                b * Mathf.Sin(f)
+            );
+
+            float distToCenter = Mathf.Sqrt(a * a - b * b);
+
+            Vector3 dirToCenterFromFoci = _rot * ((focusPoint == FocalPoint.NegativeMajorAxis) ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0));
+
+            Vector3 worldPos = (focusObject.position + distToCenter * dirToCenterFromFoci) + _rot * location;
+
+            return worldPos;
+        }
+
+        public Vector3[] GetPoints(int steps) {
+            Vector3[] points = new Vector3[steps];
+
+            for (int i = 0; i < steps; i++) {
+                points[i] = Evaluate(i * 2 * Mathf.PI / (steps - 1));
+            }
+
+            return points;
+        }
+
+        public void OnDrawGizmos() {
+            Gizmos.color = Color.white;
+
+            Vector3[] points = GetPoints(24);
+            for (int i = 0; i < points.Length; i++) {
+                int start = i;
+                int end = i == points.Length - 1 ? 0 : i + 1;
+
+                Gizmos.DrawLine(points[start], points[end]);
+            }
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(focusObject.position, Evaluate(0));
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(Evaluate(startPosition), 0.3f);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(focusNegative, 0.2f);
+            Gizmos.DrawWireSphere(focusPositive, 0.2f);
+            Gizmos.DrawWireSphere(center, 0.2f);
+        }
+
+    }
+}
